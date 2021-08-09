a01e1e9e61e544111b32f5d243aa8e90970d0a40
Firdavs468 <84624389+Firdavs468@users.noreply.github.com>
2021-08-09 14:47:43 +0500

    ready to start work

--- Valyuta/MainVC.swift
+++ Valyuta/MainVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainVC.swift
 //  Valyuta
 //
 //  Created by Firdavs Zokirov  on 09/08/21.
@@ -7,13 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
 
-
 }
-
